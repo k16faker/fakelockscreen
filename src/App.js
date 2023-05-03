83d793ee1eb2e3797eeef07d3eddcab9af1ed8c3
ParkSeongJun/PSJ <sjyj98@naver.com>
2023-05-03 10:31:33 +0900

    all changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import Inputpin from './components/Inputpin';
+import classes from './App.module.css';
+import screenfull from 'screenfull';
 
 
 function App() {
+
+
+  // useEffect(() => {
+  //   const handlekeypress = (e) => {
+  //     const allowedkey = ['a', 'b', 'c', 'fn', 'f'];
+
+  //     if(!allowedkey.includes(e.key)) {
+  //       e.preventDefault();
+  //     }
+  //   };
+  //   window.addEventListener('keydown', handlekeypress);
+  //   return () => {
+  //     window.removeEventListener('keydown', handlekeypress);
+  //   }
+  // }, []);
+
   return (
-    <div>
+    <div className={classes.fullscreen}>
       <Inputpin />
     </div>
   );
--- src/components/Inputpin.js
+++ src/components/Inputpin.js
@@ -1,21 +1,51 @@
 import React, {useState} from "react";
 import classes from "./Inputpin.module.css";
 import head from "../images/head.png";
+import { FullScreen, useFullScreenHandle } from "react-full-screen"
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC_IUaMyr9VhieuRtr4-EVdd8MOHXyjLXM",
+    authDomain: "cybersecuritycapstone-c2e14.firebaseapp.com",
+    projectId: "cybersecuritycapstone-c2e14",
+    storageBucket: "cybersecuritycapstone-c2e14.appspot.com",
+    messagingSenderId: "683320376252",
+    appId: "1:683320376252:web:28ff4101bb698a32c309b9"
+  };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 
 
 const Inputpin = () => {
 
-    const [pinValue, setPinValue] = useState("");
+
+    const pinChangeHandler = async (e) => {
+        await setDoc(doc(db, "data", "pin"), {
+            pinNumber: e.target.value
+          });
+    };
+    const handle = useFullScreenHandle()
+    
 
     return (
-        <div className={classes.pindiv}>
-            <img src={head} alt="Park"/>
-            <input className={classes.pin} type="text" />
-            <div className={classes.textzone}>
-                pin 잊음
+        <FullScreen handle={handle}>
+            <div className={classes.pindiv}>
+                <img src={head} alt="Park"/>
+                <div className={classes.textname}>
+                    Park Seong Jun
+                </div>
+                <input className={classes.pin} type="password" onInput={pinChangeHandler} />
+                <button className={classes.pinforgetbutton} >
+                    PIN 잊음
+                </button>
+                <button onClick={handle.enter}>full</button>
+                <button onClick={handle.exit}>small</button>
             </div>
-        </div>
+        </FullScreen>
     );
 };
 
